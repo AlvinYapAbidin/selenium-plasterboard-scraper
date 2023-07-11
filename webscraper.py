ec919780013dbf860b86eae3f2d6a7c9a93fe013
alvinaltus <alvin.yapabidin@altusgroup.com>
2023-07-11 12:41:37 +1000

    update scraper

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
-PLASTERBOARD_URL = "https://www.blacktownbuildingsupplies.com.au/product/"
+PLASTERBOARD_URL = "https://www.youtube.com/feed/trending"
+cert_path = "ZscalerRoot.pem"
 
-response = requests.get(PLASTERBOARD_URL)
+response = requests.get(PLASTERBOARD_URL, verify=cert_path)
 
 print('Status Cost', response.status_code)
+
+doc = BeautifulSoup(response.text, 'html.parser')
+
+print('Page title', doc.title.text)
+
+# Find all the item divs
+item_divs = doc.find_all('div', class_="ytd-video-renderer")
+
+print(f'Found {len(item_divs)} items')
